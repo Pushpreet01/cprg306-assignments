2149b57353cdfcb0f409bae1c289aaa443a156e7
Pushpreet01 <166869140+Pushpreet01@users.noreply.github.com>
2025-02-11 15:35:24 -0700

    Adding colon

--- app/page.js
+++ app/page.js
@@ -6,5 +6,5 @@ export default function Page() {
         <h1>CPRG 306: Web Development 2 - Assignments</h1>
         <Link href={"week-2"}>Week 2 Assignment</Link>
       </main>
-    )
+    );
 }
---
+++ app/week-2/student-info.js
@@ -0,0 +1,10 @@
+import Link from "next/link"
+
+export default function StudentInfo() {
+    return (
+        <main>
+            <h1>Pushpreet Singh</h1>
+            <Link href={"https://github.com/Pushpreet01/cprg306-assignments.git"}>GitHub Repo</Link>
+        </main>
+    );
+}
