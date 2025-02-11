c9371c7c7ae31637ef9b982e392858b66627807d
Pushpreet01 <166869140+Pushpreet01@users.noreply.github.com>
2025-02-11 15:38:50 -0700

    Final week 2 commit

--- app/week-2/student-info.js
+++ app/week-2/student-info.js
@@ -3,7 +3,7 @@ import Link from "next/link"
 export default function StudentInfo() {
     return (
         <main>
-            <h1>Pushpreet Singh</h1>
+            <p>Pushpreet Singh</p>
             <Link href={"https://github.com/Pushpreet01/cprg306-assignments.git"}>GitHub Repo</Link>
         </main>
     );
